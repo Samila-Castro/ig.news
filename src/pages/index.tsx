5829ba8c3373270f193eda5570dd1723637eecb5
Samila Castro <samilav1997@gmail.com>
2022-01-04 15:50:27 -0300

    remove import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
      //Anexado ao Head do document -- next
@@ -9,7 +7,7 @@ export default function Home() {
      <Head>
      <title>Início | ig.news</title>
    </Head>
-    <h1 className={styles.title}>Hello,world!</h1>
+    <h1>Hello,world!</h1>
     </> 
   )
 }
